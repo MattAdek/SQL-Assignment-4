2a2f3dc80a6d81cbb96b3408925cf991cbca0459
Adekunle Oluwatoba Matthew <139861838+MattAdek@users.noreply.github.com>
2025-07-24 00:11:17 +0100

    Add files via upload

---
+++ MY SQL work 11.sql
@@ -0,0 +1,36 @@
+USE samplestore;
+
+SELECT *
+FROM `employee attrition`
+;
+
+
+-- Method 1
+SELECT 
+		ROUND(
+        (SELECT COUNT(MaritalStatus)
+        FROM `employee attrition`
+        WHERE MaritalStatus = 'Divorced') 
+        * 100
+        /
+        (SELECT COUNT(MaritalStatus)
+        FROM `employee attrition`),2)
+         AS PercentageDivorced
+        ;
+        
+-- Method 2
+WITH Total_employees AS (
+						 SELECT 
+                         COUNT(MaritalStatus) AS TotalEmployees
+                         FROM `employee attrition`
+                         )
+						SELECT
+							ROUND(
+                            (SELECT 
+                            COUNT(MaritalStatus)
+                            FROM `employee attrition`
+                            WHERE MaritalStatus = 'Divorced') * 100
+                            /
+                            TotalEmployees,2) AS Percentage_Divorced
+							FROM Total_employees
+                            ;
